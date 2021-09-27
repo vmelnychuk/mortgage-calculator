f01fce80eac18e3198f8a2b37a79aab119b052c6
Vasyl Melnychuk <vmmelnychuk@gmail.com>
2021-09-27 21:28:45 +0300

    update files from react template

--- mortgage-calculator-client/src/App.test.js
+++ mortgage-calculator-client/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Mortgage Calculator', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const logo = screen.getByText(/Mortgage Calculator/i);
+  expect(logo).toBeInTheDocument();
 });
